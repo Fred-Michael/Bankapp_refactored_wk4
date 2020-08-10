010709e0b94a3f64d3781dce94477c657163a204
Fred-Michael <chinazorfredrick@gmail.com>
2020-08-10 21:48:34 +0100

    Add project files.

---
+++ Bankapp_refactored_week/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankapp_refactored_week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassLibrary/BankStore/BankData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BankClassLibraries
+{
+    public class BankData
+    {
+        //This BankData class holds the lists of Accounts, Customers and Transactions
+        public static List<Accounts> Accounts { get; set; } = new List<Accounts>();
+        public static List<Customers> Customers { get; set; } = new List<Customers>();
+        public static List<Transactions> Transactions { get; set; } = new List<Transactions>();
+    }
+}
